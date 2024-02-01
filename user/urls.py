df56b1a74787a3a6c70e00da502bd7192ef7ab26
fbdav <brad.davison@novamacro.net>
2024-02-01 11:38:39 -0600

    Login Functionality Completed

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='index'),
     path('login/', views.login_user, name='login'),
+
     ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from user.form import AccountAuthenticationForm
 from django.contrib.auth.models import Group
 from django.http import HttpResponse
@@ -7,18 +8,33 @@ from django.http import HttpResponse
 
 def login_user(request):
     user = request.user
+    fail = False
     if user.is_authenticated:
-        return redirect('tier_welcome')
+        return redirect('index')
     if request.method == 'POST':
         form = AccountAuthenticationForm(request.POST)
         if form.is_valid():
             email = request.POST['email']
             password = request.POST['password']
             user = authenticate(email=email, password=password)
+            print(user)
             if user:
                 login(request, user)
             return redirect('index')
+        else:
+            fail = True
+            print(form.is_valid())
+            print(fail)
+            context = {'form': form, 'fail': fail}
+            return render(request, 'user/login.html', context)
     else:
         form = AccountAuthenticationForm()
-    context = {'form': form}
+    context = {'form': form, 'fail': fail}
     return render(request, 'user/login.html', context)
+
+
+@login_required()
+def home(request):
+    title = 'Portal - Home'
+    context = {'title': title}
+    return render(request, 'index.html', context)
